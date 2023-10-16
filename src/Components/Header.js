c0b7d6ac20d6125e2993dcbc17295b6f8ed00ae0
piber-dev <piber.dev@icloud.com>
2023-10-16 17:11:15 +0200

    remove logo from header on mobile

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,22 @@
+import MediaQuery from 'react-responsive'
+
+function Logo() {
+  return <div name='logo' className='absolute left-0'>
+          <div className='mt-4 ml-20'>
+            LOGO
+          </div>
+         </div>
+}
+
 function Header() {
   return <header className="header flex h-16 border-b-[1px] border-[#E5E7EB]">
-    <div name='logo' className='absolute left-0'>
-      <div className='mt-4 ml-20'>
-        LOGO
-      </div>
-    </div>
+    <MediaQuery minWidth={1224}>
+      {(matches) =>
+        matches
+          ? <Logo />
+          : <></>
+      }
+    </MediaQuery>
     <div className='absolute right-0 text-lg text-[#1F2937]'>
       <div name='navigation' className='mt-4 flex mr-10'>
         <div name='nav-1' className='px-4'>
